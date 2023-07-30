13164b781fd4489c0fd5556f8726711d2b184d9d
Aftab <aftabansari37786@gmail.com>
2023-07-30 16:46:37 +0530

    files added

---
+++ Smallest_No_Using_Sort_Method.java
@@ -0,0 +1,18 @@
+public class Smallest_No_Using_Sort_Method {
+    public static void main(String[] args) {
+        int[] arr = {3,21,55,65,24,75,76,32,54,64,67,88,98,97,97,5,78,69965,865,556,8544,3,355};
+//        int[] smallno = sort(arr);
+        System.out.println(sort(arr));
+    }
+
+    static int sort (int[] no){
+        int min = no[0];
+        for (int i=0; i < no.length; i++){
+            if (no[i] < min) {
+                min = no[i];
+            }
+        }
+        return min;
+
+    }
+}
